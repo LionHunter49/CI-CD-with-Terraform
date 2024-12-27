678f25e69d5372a8ff3914efd0db2d47892a88f4
LionHunter49 <araxkohar@gmail.com>
2024-12-27 06:16:23 -0500

    get response

--- nodeapp_source_codes/index.js
+++ nodeapp_source_codes/index.js
@@ -9,3 +9,11 @@ app.get('/', (req, res) => {
 app.get('/services/service-1', (req, res) => {
   res.send('This is a sample response from service 1 (Nodejs App Service) v4')
 })
+
+app.get('/services/service-1/status', (req, res) => {
+  res.send('ok')
+})
+
+app.listen(port, () => {
+console.log(`Example app listening at http://localhost:${port}`)
+})
