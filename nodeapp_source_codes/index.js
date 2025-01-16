eddbfd366b30b911ebca4031a317f948dfaea56d
LionHunter49 <araxkohar@gmail.com>
2025-01-16 08:32:09 -0500

    Create README file

--- nodeapp_source_codes/index.js
+++ nodeapp_source_codes/index.js
@@ -9,11 +9,10 @@ app.get('/', (req, res) => {
 app.get('/services/service-1', (req, res) => {
   res.send('This is a sample response from service 1 (Nodejs App Service) v4')
 })
-
 app.get('/services/service-1/status', (req, res) => {
-  res.send('ok')
-})
+    res.send('ok')
+  })
 
 app.listen(port, () => {
-console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`)
 })
